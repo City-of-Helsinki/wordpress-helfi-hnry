a5439b37ae65e308ce688ad124e96a024bf6b770
ssorjonen <sorjonen.santeri@gmail.com>
2022-06-22 14:07:55 +0300

    WCAG improvements; (#1)
    Add necessary translation capabilities
    Add required attributes for proper keyboard usage
    Fix color contrast issues
    Disable unnecessary links
    Other usability changes

---
+++ searchform.php
@@ -0,0 +1,13 @@
+<?php
+  $search_input_aria = function_exists('pll__') ? pll__('Syötä hakusana haettaville ratkaisuille') : 'Syötä hakusana haettaville ratkaisuille';
+  $search_button_aria = function_exists('pll__') ? pll__('Hae ratkaisuja') : 'Hae ratkaisuja';
+
+?>
+
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <div>
+    <label class="screen-reader-text" for="s"><?php echo _x( 'Search for:', 'label' ); ?></label>
+    <input placeholder="<?php echo $search_input_aria ?>" type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+    <input aria-label="<?php echo $search_button_aria ?>"type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
+  </div>
+</form>
--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<main class="main-single">
+<main id="main" class="main-single">
     <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
     <?php endwhile; else: endif; ?>
